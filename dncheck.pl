074d6c00958efa834c02c116cc06c6fa4fd2a037
Giovanni Bechis <giovanni@bigio.snb.it>
2013-10-08 22:12:25 +0200

    do not print anything on functions, only on main()

--- dncheck.pl
+++ dncheck.pl
@@ -8,11 +8,9 @@ use Net::DNS;
 my $domain = shift;
 my $res   = Net::DNS::Resolver->new;
 
-print &find_ns($domain);
-
 sub resolve_host() {
  my $host = shift;
- my $result;
+ my @result;
  my $rr;
  my $count = 0;
  my $query = $res->search($host);
@@ -20,31 +18,45 @@ sub resolve_host() {
  if ($query) {
   foreach my $rr ($query->answer) {
    next unless $rr->type eq "A";
-   $result .= $rr->address;
-   if ($count > 0) {
-    $result .= " - ";
-    $count++;
-   }
+   $result[$count] = $rr->address;
+   $count++;
   }
  } else {
   warn "query failed: ", $res->errorstring, "\n";
  }
- return $result;
+ return @result;
 }
 
 sub find_ns() {
  my $domain = shift;
- my $result;
+ my @result;
+ my @resolved;
  my $rr;
+ my $count = 0;
 
  my $query = $res->query($domain, "NS");
 
  if ($query) {
   foreach $rr (grep { $_->type eq 'NS' } $query->answer) {
-   $result .= "NS: " . $rr->nsdname . " ( " . &resolve_host($rr->nsdname) ." )\n";
+   $result[$count]{'NS'} = $rr->nsdname;
+   @resolved = &resolve_host($rr->nsdname);
+   for my $i ( 0 .. (@resolved - 1) ) {
+	$result[$count]{'NS_RESOLVED'} = $resolved[$i];
+   }
+   $count++;
   }
  } else {
   warn "query failed: ", $res->errorstring, "\n";
  }
- return $result;
+ return @result;
+}
+
+my @ns = &find_ns($domain);
+for my $i ( 0 .. @ns ) {
+ if ( defined $ns[$i] ) {
+  print $ns[$i]{'NS'};
+  print " ( ";
+  print $ns[$i]{'NS_RESOLVED'};
+  print " )\n";
+ }
 }
