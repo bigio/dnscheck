761908a212be1e3ebed52995f76765a59f1f87b8
Giovanni Bechis <giovanni@bigio.snb.it>
2013-10-08 18:49:06 +0200

    Print ip address of discovered NS records

--- dncheck.pl
+++ dncheck.pl
@@ -10,6 +10,28 @@ my $res   = Net::DNS::Resolver->new;
 
 print &find_ns($domain);
 
+sub resolve_host() {
+ my $host = shift;
+ my $result;
+ my $rr;
+ my $count = 0;
+ my $query = $res->search($host);
+
+ if ($query) {
+  foreach my $rr ($query->answer) {
+   next unless $rr->type eq "A";
+   $result .= $rr->address;
+   if ($count > 0) {
+    $result .= " - ";
+    $count++;
+   }
+  }
+ } else {
+  warn "query failed: ", $res->errorstring, "\n";
+ }
+ return $result;
+}
+
 sub find_ns() {
  my $domain = shift;
  my $result;
@@ -19,7 +41,7 @@ sub find_ns() {
 
  if ($query) {
   foreach $rr (grep { $_->type eq 'NS' } $query->answer) {
-   $result .= "NS: " . $rr->nsdname . "\n";
+   $result .= "NS: " . $rr->nsdname . " ( " . &resolve_host($rr->nsdname) ." )\n";
   }
  } else {
   warn "query failed: ", $res->errorstring, "\n";
