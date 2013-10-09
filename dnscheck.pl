953e9428b831b1bd02616ee89139cb7ed5255cd6
Giovanni Bechis <giovanni@bigio.snb.it>
2013-10-09 09:07:36 +0200

    Fix after renamed file, re-add proper function calls to dnscheck::dns module

--- dnscheck.pl
+++ dnscheck.pl
@@ -6,7 +6,7 @@
 use strict;
 use warnings;
 
-use Net::DNS;
+use dnscheck::dns;
 
 my $domain = shift;
 my $resolver = shift;
@@ -15,52 +15,6 @@ my $pub_res;
 
 if ( not defined $domain ) {
  die "usage: dnscheck domain [resolver]\n";
-}
-
-
-# Resolve an host, returns an arrays of corresponding ip
-sub resolve_host() {
- my $host = shift;
- my @result;
- my $rr;
- my $count = 0;
- my $query = $pub_res->search($host);
-
- if ($query) {
-  foreach my $rr ($query->answer) {
-   next unless $rr->type eq "A";
-   $result[$count] = $rr->address;
-   $count++;
-  }
- } else {
-  warn "query failed: ", $pub_res->errorstring, "\n";
- }
- return @result;
-}
-
-# Find authoritative name servers
-sub find_ns() {
- my $domain = shift;
- my @result;
- my @resolved;
- my $rr;
- my $count = 0;
-
- my $query = $auth_res->query($domain, "NS");
-
- if ($query) {
-  foreach $rr (grep { $_->type eq 'NS' } $query->answer) {
-   $result[$count]{'NS'} = $rr->nsdname;
-   @resolved = &resolve_host($rr->nsdname);
-   for my $i ( 0 .. (@resolved - 1) ) {
-	$result[$count]{'NS_RESOLVED'} = $resolved[$i];
-   }
-   $count++;
-  }
- } else {
-  warn "query failed: ", $auth_res->errorstring, "\n";
- }
- return @result;
 }
 
 # Main function
@@ -79,7 +33,7 @@ if ( defined $resolver ) {
 $pub_res = Net::DNS::Resolver->new;
 
 # Find ns and print them with their ip
-my @ns = &find_ns($domain);
+my @ns = &dnscheck::dns::find_ns($domain, $auth_res, $pub_res);
 for my $i ( 0 .. @ns ) {
  if ( defined $ns[$i] ) {
   print $ns[$i]{'NS'};
