b0e2b24dfa85a007cd5da9ac03ea4612553dcff4
Giovanni Bechis <giovanni@bigio.snb.it>
2013-11-19 19:45:32 +0100

    Better output formatting Prepare for verbose mode

--- dnscheck.pl
+++ dnscheck.pl
@@ -14,6 +14,7 @@ use dnscheck::parse;
 
 my $domain = shift;
 my $resolver = shift;
+my $verbose = 0;
 my $auth_res;
 my $pub_res;
 my %soa_info;
@@ -39,6 +40,7 @@ $pub_res = Net::DNS::Resolver->new;
 
 # Find ns and print them with their ip
 my @ns = &dnscheck::dns::find_ns($domain, $auth_res, $pub_res);
+print "NS records for domain " . $domain . "\n";
 for my $i ( 0 .. @ns ) {
  if ( defined $ns[$i] ) {
   print $ns[$i]{'NS'};
@@ -47,12 +49,27 @@ for my $i ( 0 .. @ns ) {
   print " )\n";
  }
 }
+print "\n";
+print "record for domain should not be a CNAME...\t";
 if(&dnscheck::dns::check_cname($domain, $auth_res) ) {
-  print "Error: record for domain $domain is a CNAME record\n";
+  print "KO\n";
+} else {
+  print "OK\n";
 }
 %soa_info = &dnscheck::parse::parse_soa(&dnscheck::dns::find_soa($domain, $auth_res));
+print "soa record checks...\t\t\t\t";
 if ( not defined $soa_info{NS} ) {
-  print "Error: soa query has failed\n";
+  print "KO";
+  if ( $verbose ) {
+    print " (soa query has failed)";
+  }
+  print "\n";
 } elsif ( ( Net::IP::ip_is_ipv4($soa_info{NS}) ) ) {
-  print "Error: nameserver in soa record should not be an ip address\n";
+  print "KO";
+  if ( $verbose ) {
+    print " (nameserver in soa record should not be an ip address)";
+  }
+  print "\n";
+} else {
+  print "OK\n";
 }
