283224d14475ba80cff1233c18baef31a59e43a3
Giovanni Bechis <giovanni@bigio.snb.it>
2015-09-26 11:41:28 +0200

    Unbreak SOA record parsing

--- dnscheck/parse.pm
+++ dnscheck/parse.pm
@@ -12,16 +12,18 @@ sub parse_soa() {
 	my $soa_record = shift;
 	my $tmp_soa_rec = '';
 	my @a_tmp_soa;
-	my @a_soa;
+	my @a_soa_l1;
+	my @a_soa_l2;
 	my %soa_info;
 
 	@a_tmp_soa = split('\(', $soa_record);
-	@a_soa = split(' ', $a_tmp_soa[0]);
- 
+	@a_soa_l1 = split(' ', $a_tmp_soa[0]);
+	@a_soa_l2 = split(' ', $a_tmp_soa[1]);
+
 	# Create an hash with some infos of the SOA record
-	$soa_info{DOMAIN} = $a_soa[0];
-	$soa_info{NS} = $a_soa[4];
-	$soa_info{EMAIL} = $a_soa[5];
+	$soa_info{DOMAIN} = $a_soa_l1[0];
+	$soa_info{NS} = $a_soa_l2[4];
+	$soa_info{EMAIL} = $a_soa_l2[6];
 
 	return %soa_info;
 }
